59eeedce33135ddea8b470e4dc3a959005f75e9e
Leon363 <tuea1982@gmail.com>
2022-10-08 15:26:42 +0300

    HW_3-5

---
+++ HW3-5/src/telran/numbers/SportLotto_87_APP.java
@@ -0,0 +1,35 @@
+package telran.numbers;
+
+public class SportLotto_87_APP {
+	
+	public class SportLotoAppl {
+
+		private static final int N_NUMBERS = 7;
+		private static final int MIN_NUMBER = 1;
+		private static final int MAX_NUMBER = 49;
+		public static void main(String[] args) {
+			int numbers[] = new int[N_NUMBERS];
+			for (int i = 0; i < N_NUMBERS; i++) {
+				int number = getUniqueNumber(numbers, MIN_NUMBER, MAX_NUMBER);
+				System.out.print(number + " ");
+				numbers[i] = number;
+			}
+			
+
+		}
+		private static int getUniqueNumber(int[] ar, int minNumber, int maxNumber) {
+			int res = 0;
+			do {
+				res = getRandomNumber(minNumber, maxNumber);
+			} while(ArrayInt.indexOf(ar, res) >= 0);
+			return res;
+		}
+		
+		private static int getRandomNumber(int min, int max) {
+			return (int) (min + Math.random() * (max - min + 1));
+		}
+
+	}
+
+
+}
